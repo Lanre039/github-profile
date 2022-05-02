e95006ae248b6a83fdb598c5d0f46ca0a56e01e4
Lanre039 <rajilat1000@gmail.com>
2022-05-02 13:53:36 +0100

    changed dir

--- server/index.js
+++ server/index.js
@@ -13,10 +13,10 @@ app.use(bodyParser.json({ type: "text/*" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../build")));
 
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../build", "index.html"));
   });
 }
 
